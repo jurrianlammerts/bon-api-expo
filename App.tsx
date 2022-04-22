e6698f539f99c933b085e7c64b6babbcd5f69bd9
Jurrian <jurrianlammerts@hotmail.com>
2022-04-22 13:02:50 +0200

    Initial setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, View, StyleSheet } from "react-native";
+import * as WebBrowser from "expo-web-browser";
 
 export default function App() {
+  const [data, setData] = useState<{
+    id: string;
+    link: string;
+    isActive: boolean;
+  }>();
+
+  // call the fetch function on mount with the id: "123"
+  useEffect(() => {
+    getData("123").then((items) => {
+      // save the json response in state
+      setData(items);
+    });
+  }, []);
+
+  const _handlePressButtonAsync = async () =>
+    // open the link in a in-app browser
+    await WebBrowser.openBrowserAsync("https://www.bon-api.com/");
+
+  const getData = (id: string) =>
+    // fetch the recipe data from the Bon API
+    fetch(`https://bon-api.com/api/v1/recipes/${id}`).then((data) =>
+      data.json()
+    );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Order recipe"
+        // disabled={!data.isActive} -> disable button if the recipe link is not available
+        onPress={_handlePressButtonAsync}
+        color="#259137"
+      />
     </View>
   );
 }
@@ -13,8 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
