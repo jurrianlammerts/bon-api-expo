061878afef7ac034270d1a022ae55e38b2951329
Jurrian <jurrianlammerts@hotmail.com>
2022-04-25 20:04:17 +0200

    Changed api call

--- App.tsx
+++ App.tsx
@@ -2,36 +2,66 @@ import React, { useState, useEffect } from "react";
 import { Button, View, StyleSheet } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 
+const process = {
+  env: {
+    userName: "d47751abd4e8f72e",
+    password:
+      "9eb993127fc7bd06b21ab13995dc04435afa136d19d1fc7666113cff45701156",
+    authHeader:
+      "hizhdQE8OG4TYfAOaIpMlIUkidIU1O5SQJWhCfk:T2arF7wXKrooUWzYI64tw8mdjg376zmJ439CawrZ3qt3x9WHCSsJNywYw0D5XMjsbwhFCtLKVleUhLnDctvCzUCD6MQF46MA4z3qqN1iNl7evAHTjAJSPsZmAQAhdD14",
+  },
+};
+
 export default function App() {
-  const [data, setData] = useState<{
-    id: string;
-    link: string;
-    isActive: boolean;
+  const [recipeData, setRecipeData] = useState<{
+    grocery_widget_url: string;
+    delivery_partner_logos: string;
+    show_widget_button: boolean;
   }>();
 
-  // call the fetch function on mount with the id: "123"
+  // call the fetch function on mount with the id
   useEffect(() => {
-    getData("123").then((items) => {
-      // save the json response in state
-      setData(items);
-    });
+    async function fetchData() {
+      const response = await getRecipeData("11004", "Widget inApp Testing");
+      setRecipeData(response);
+    }
+    fetchData();
   }, []);
 
-  const _handlePressButtonAsync = async () =>
+  const _handlePressButtonAsync = async () => {
+    const url = recipeData?.grocery_widget_url;
     // open the link in a in-app browser
-    await WebBrowser.openBrowserAsync("https://www.bon-api.com/");
+    if (url) {
+      await WebBrowser.openBrowserAsync(url);
+    }
+  };
 
-  const getData = (id: string) =>
+  const getRecipeData = async (id: string, recipeName: string) => {
     // fetch the recipe data from the Bon API
-    fetch(`https://bon-api.com/api/v1/recipes/${id}`).then((data) =>
-      data.json()
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      Authorization: `Basic ${process.env.authHeader}`,
+      body: JSON.stringify({
+        userName: process.env.userName,
+        password: process.env.password,
+        recipeID: id,
+        recipeName: recipeName,
+      }),
+    };
+    return await fetch("https://bon-api.com/o/token/", requestOptions).then(
+      (data) => data.json()
     );
+  };
 
   return (
     <View style={styles.container}>
       <Button
         title="Order recipe"
-        // disabled={!data.isActive} -> disable button if the recipe link is not available
+        // disable button if the recipe link is not available
+        disabled={!recipeData?.show_widget_button}
         onPress={_handlePressButtonAsync}
         color="#259137"
       />
